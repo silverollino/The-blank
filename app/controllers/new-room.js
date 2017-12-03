95cf41aba9752110535b1b12a2883261c996accf
Worki <30875329+silverollino@users.noreply.github.com>
2017-12-03 02:12:48 -0600

    Harto

--- app/controllers/new-room.js
+++ app/controllers/new-room.js
@@ -1,24 +1,13 @@
 import Controller from '@ember/controller';
 import Ember from 'ember';
+//import sweetAlert from 'ember-sweetalert';
+import { debug } from '@ember/debug';
+//import FindQuery from 'ember-emberfire-find-query/mixins/find-query';
+
+//window.EmberENV = window.EmberENV || {};
+//window.EmberENV.ENABLE_DS_FILTER = true;
 
 export default Controller.extend({
     store: Ember.inject.service("store"),
-    actions:{
-        newRoom(){
-            //Inicializamos el modelo
-            let room = this.get("store").createRecord('cuarto',{});
-
-            room.set("roomCode", this.get("roomName"));
-            //Validamos que se haya introducido un codigo de cuarto
-            if(Ember.isBlank(room.get("roomCode"))){
-                alert("Debe de introducir un codigo de cuarto antes de seguir");
-                return;
-            }
-
-            room.save().then(()=>{
-                alert("Cuarto NOMBRE ha sido creado ahora unete");
-                this.set("roomCreated", true);
-            });
-        }
-    }
+    
 });
