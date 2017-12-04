39d86f021ef566306616c532ee91750eaec3d37c
Worki <30875329+silverollino@users.noreply.github.com>
2017-12-04 00:04:11 -0600

    terminado verificacion de cuarto nuevo

--- app/controllers/new-room.js
+++ app/controllers/new-room.js
@@ -9,5 +9,72 @@ import { debug } from '@ember/debug';
 
 export default Controller.extend({
     store: Ember.inject.service("store"),
+
+    actions: {
+        newRoom(rname, model) {
+            //Obtenemos el modelo con el parametro "model" para asi poder leer los datos que tiene dentro
+
+            //Validamos que se haya introducido un codigo de cuarto
+            if (Ember.isBlank(rname)) {
+                sweetAlert({
+                    type: 'error',
+                    title: 'No puede dejar en blanco el Codigo de cuarto',
+                    showConfirmButton: false,
+                    timer: 2000
+                });
+                return;
+            }
+
+            //Si no se dejo en blanco entonces
+            let room = this.get("store").createRecord('cuarto', {});
+            room.set("roomCode", rname);
+
+            var currentRooms = model.cuartos.content; //Obtenemos el arreglos con datos a leer         
+            var repetido;
+            var seRepitio = false;
+
+            //Obteniendo el tamaño del arreglo
+            if (typeof (currentRooms) == "undefined" || typeof (currentRooms) == "string" || typeof (currentRooms) == "number") {
+                return repetido = 0;
+            } else {
+                repetido = currentRooms.length;
+            }
+            
+            //Checamos cada entrada del arreglo para ver si existe un cuarto con ese nombre
+            for (let i = 0; i < repetido; i++) {
+                
+                let romName = currentRooms[i]._data.roomCode;
+                if (romName == rname) {                    
+                    seRepitio = true;                    
+                    break;
+                }
+            }
+            
+            //SI en efecto existe un cuarto con el nombre regresamos
+            if (seRepitio) {
+                sweetAlert({
+                    type: 'error',
+                    title: 'Ya existe',
+                    text: 'Este codigo ya existe usa otro',
+                    showConfirmButton: false,
+                    timer: 2500
+                });
+                return;
+            } else {
+
+                //Si no existe un cuarto con ese numero salvamos en la BD              
+                room.save().then(() => {
+                    this.set("roomCreated", true);
+                    sweetAlert({
+                        type: 'success',
+                        title: 'Se ha creado un cuarto con el nombre: ' + room.get("roomCode"),
+                        showConfirmButton: false,
+                        timer: 3000
+                    });
+                    
+                });
+            }
+        }
+    },
     
 });
--- app/routes/new-room.js
+++ app/routes/new-room.js
@@ -2,78 +2,15 @@ import Route from '@ember/routing/route';
 import Ember from 'ember';
 import sweetAlert from 'ember-sweetalert';
 import RSVP from 'rsvp';
+import { debug } from '@ember/debug';
 
 export default Route.extend({
     store: Ember.inject.service("store"),
 
     model() {
         return RSVP.hash({
-           cuartos: this.get('store').findAll('cuarto')
+            cuartos: this.get('store').findAll('cuarto')
         });
-        
-    },
-
-    actions: {
-        newRoom(rname) {
-            
-        //     let arreglo = this.store.findAll('cuarto').then(data => {
-        //         let result = [];
-        //         data.forEach(element => {
-        //             let record = element.get('data');
-        //             record.id = element.get('id');
-        //             result.push(record);
-        //         });
-        //         console.log(result);
-        //         this.set('arreglo', result);
-        //     })
-        //     console.log(arreglo);
-        //    console.log(this.get(result))
-            //Validamos que se haya introducido un codigo de cuarto
-            if (Ember.isBlank(rname)) {
-                sweetAlert({
-                    type: 'error',
-                    title: 'No puede dejar en blanco el Codigo de cuarto',
-                    showConfirmButton: false,
-                    timer: 2000
-                });
-                return;
-            }
-            else {
-                // let repetido = this.store.query('cuarto', {
-                //         orderBy: 'roomCode',
-                //         equalTo: rname
-                //     });
-
-                // let room = this.get("store").createRecord('cuarto', {});
-                // room.set("roomCode", rname);
-
-                // if (repetido.lenght >= 1) {
-                //     sweetAlert({
-                //         type: 'error',
-                //         title: 'Ya existe',
-                //         text: 'Este codigo ya existe usa otro',
-                //         showConfirmButton: false,
-                //         timer: 2500
-                //     });
-                //     return;
-                // } else {
-                    //Inicializamos el modelo
-
-                    //Validamos que se haya introducido un codigo de cuarto                    
-                    room.save().then(() => {
-
-                        sweetAlert({
-                            type: 'success',
-                            title: 'Se ha creado un cuarto con el nombre: ' + room.get("roomCode"),
-                            showConfirmButton: false,
-                            timer: 3000
-                        });
-                        this.set("roomCreated", true);
-                    });
-                //}
-
-            }
-        }
     },
 });
 
